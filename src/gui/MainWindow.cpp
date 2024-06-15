0a03ee87e78c3fae8269c721e5162a4f42ddd0a3
Zachary Freed <theconfuzzleddude@gmail.com>
2024-06-15 21:59:25 +0100

    Wayland autotype implementation (using xdg-desktop-portal)

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -1944,6 +1944,11 @@ void MainWindow::lockAllDatabases()
     m_ui->tabWidget->lockDatabases();
 }
 
+void MainWindow::requestGlobalAutoType(const QString& search)
+{
+    emit osUtils->globalShortcutTriggered("autotype", search);
+}
+
 void MainWindow::displayDesktopNotification(const QString& msg, QString title, int msTimeoutHint)
 {
     if (!m_trayIcon || !QSystemTrayIcon::supportsMessages()) {
